a14d016cef6fafab93709d6b9f7f3f502d817075
Tuan Kiet <tuankiet.luong@foody.vn>
2021-02-26 14:38:54 +0700

    refortmat code

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,19 +18,56 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
+import com.example.androiddevchallenge.model.PuppyModel
+import com.example.androiddevchallenge.model.puppies
+import com.example.androiddevchallenge.ui.components.PuppyCardView
+import com.example.androiddevchallenge.ui.components.PuppyDetail
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             MyTheme {
-                MyApp()
+                NavHost(navController = navController, startDestination = "home") {
+                    composable("home") {
+                        HomePage(data = puppies, navController = navController)
+                    }
+
+                    composable(
+                        "detail/{puppyId}",
+                        arguments = listOf(
+                            navArgument("puppyId") {
+                                type = NavType.IntType
+                            }
+                        )
+                    ) {
+                        PuppyDetail(
+                            data = puppies.first { puppy ->
+                                puppy.id == it.arguments!!.getInt(
+                                    "puppyId"
+                                )
+                            }
+                        )
+                    }
+                }
             }
         }
     }
@@ -38,9 +75,18 @@ class MainActivity : AppCompatActivity() {
 
 // Start building your app here!
 @Composable
-fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+fun HomePage(data: List<PuppyModel>, navController: NavController) {
+    Column {
+        TopAppBar(title = { Text(text = "Puppy Adoption") })
+        Surface(color = MaterialTheme.colors.background) {
+            LazyColumn {
+                items(data) {
+                    PuppyCardView(data = it) { puppy ->
+                        navController.navigate("detail/${puppy.id}")
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -48,7 +94,7 @@ fun MyApp() {
 @Composable
 fun LightPreview() {
     MyTheme {
-        MyApp()
+        HomePage(puppies, rememberNavController())
     }
 }
 
@@ -56,6 +102,6 @@ fun LightPreview() {
 @Composable
 fun DarkPreview() {
     MyTheme(darkTheme = true) {
-        MyApp()
+        HomePage(puppies, rememberNavController())
     }
 }
